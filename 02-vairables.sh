1626af0e21b6e09a1bb27a8e1f1dcd8bcafa2e38
Giridhar052020 <giridhar.maram20@gmail.com>
2023-12-25 19:08:52 +0530

    This is my secound notes

---
+++ 02-vairables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo " Hello Honey, Good Morning"
+
+echo " HI DAD, very good morning"
+
+echo " Hi Honey, How are you"
+
+echo " HI DAD, I am fine, How about you"
+
+echo " Hi honey. I am fine. thanks"
