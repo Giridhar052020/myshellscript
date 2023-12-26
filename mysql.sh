a3eaa333cba3617232756e5db9cbd4b09adf957b
Giridhar052020 <giridhar.maram20@gmail.com>
2023-12-26 21:07:55 +0530

    added

--- mysql.sh
+++ mysql.sh
@@ -5,12 +5,15 @@ ID=$(id -u)
 
 if [ $ID -eq 0 ]
 then
-    echo "Success : I having root access to install application"
+    echo "Success : Your are having root access to install application"
 else
-    echo "Failure : I Don't have access to install application"
+    echo "Failure : You Don't have access to install application"
+
+    exit 1
+
 fi # END the Conidtion
 
-exit 1
+
 
 yum install mysql -y # YUM will download and install the application
 
