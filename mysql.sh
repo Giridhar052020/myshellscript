ac897b41f561347db29a9088961d286224c83068
Giridhar052020 <giridhar.maram20@gmail.com>
2023-12-26 21:00:31 +0530

    sql installation script

---
+++ mysql.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# MySQL Installation
+
+ID=$(id -u)
+
+if [ ID -eq 0 ]
+then
+    echo "Success : I having root access to install application"
+else
+    echo "Failure : I Don't have access to install application"
+fi # END the Conidtion
+
+yum install mysql -y # YUM will download and install the application
+
+
+
